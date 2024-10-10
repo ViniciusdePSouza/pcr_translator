ccccac3312d8b74813d0c374847c6f2e62ace4ed
Vinícius Souza <vinicius.souza@appvisto.com>
2024-10-10 15:43:12 -0300

    styles: configuration tab done

--- src/app/userconfig/page.tsx
+++ src/app/userconfig/page.tsx
@@ -5,16 +5,77 @@ import { Modal } from "@/components/Modal";
 
 import { useRouter } from "next/navigation";
 
-import { Container, Content } from "./styles";
+import {
+  Container,
+  Content,
+  TabContent,
+  TabList,
+  TabRoot,
+  TabTrigger,
+} from "./styles";
+import { Button } from "@/components/Button";
+import { CustomInput } from "@/components/CustomInput";
+import { TextArea } from "@/components/TextArea";
+import { FileHtml, Key } from "phosphor-react";
+
+type ActiveTabType = "apiKeys" | "html";
 
 export default function UserConfig() {
   const navigator = useRouter();
 
+  function handleSave(activeTab: ActiveTabType) {
+    console.log(activeTab);
+  }
+
+  const TabComponent = () => {
+    return (
+      <>
+        <TabRoot defaultValue="apiKeys">
+          <TabList>
+            <TabTrigger value="apiKeys" children={<Key size={32} />} />
+            <TabTrigger value="html" children={<FileHtml size={32} />} />
+          </TabList>
+          <TabContent value="apiKeys">
+            <CustomInput
+              placeholder={"Please inform your zero bounce api key"}
+              label={"Zero Bounce Api Key"}
+            />
+            <CustomInput
+              placeholder={"Please inform your Open AI api key"}
+              label={"Open AI API Key"}
+            />
+            <Button
+              title={"Save"}
+              isLoading={false}
+              onClick={() => {
+                handleSave("apiKeys");
+              }}
+            />
+          </TabContent>
+
+          <TabContent value="html">
+            <TextArea
+              label={"Html Pattern"}
+              placeholder={"<span>This is a html example</span>"}
+            />
+            <Button
+              title={"Save"}
+              isLoading={false}
+              onClick={() => {
+                handleSave("html");
+              }}
+            />
+          </TabContent>
+        </TabRoot>
+      </>
+    );
+  };
+
   return (
     <Container>
-      <Header title={"Filter Email Domain!"} />
+      <Header title={"Account Preferences"} />
       <Content>
-        <Modal content={<h1>OI</h1>} />
+        <Modal content={<TabComponent />} />
       </Content>
     </Container>
   );
--- src/app/userconfig/styles.ts
+++ src/app/userconfig/styles.ts
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { Tabs } from "@radix-ui/themes";
 
 export const Container = styled.div`
   width: 100%;
@@ -9,19 +10,68 @@ export const Container = styled.div`
   align-items: center;
   min-height: 100vh;
   min-width: 100vw;
-
-  background-color: ${({ theme }) => theme.COLORS.WHITE_100};
 `;
 
 export const Content = styled.div`
   width: 100%;
   padding: 2.4rem 1.2rem;
   min-height: 100vh;
-
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 
   background-color: ${({ theme }) => theme.COLORS.GRAY_50};
+`;
+
+export const TabRoot = styled(Tabs.Root)``;
+
+export const TabList = styled(Tabs.List)`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  padding: 1.2rem 1.2rem 0 1.2rem;
+
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+
+  gap: 8px;
+
+  overflow: scroll;
+  scroll-behavior: smooth;
+
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY};
+`;
+
+export const TabTrigger = styled(Tabs.Trigger)`
+  all: unset;
+  padding: 0.4rem 1.2rem;
+
+  border-top-right-radius: 4px;
+  border-top-left-radius: 4px;
+
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY};
+
+  color: ${({ theme }) => theme.COLORS.WHITE_100};
+  font-size: 1.6rem;
+  font-weight: 700;
+
+  &[data-state="active"] {
+    border-bottom: 3px solid ${({ theme }) => theme.COLORS.SECONDARY};
+
+  }
+`;
+
+export const TabContent = styled(Tabs.Content)`
+  padding: 1.2rem 2.4rem;
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY};
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
 `;
